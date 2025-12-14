3fc69122e7d57b1b733551d259d014ab3afc6112
8g6ghfrgj <152427918+8g6ghfrgj@users.noreply.github.com>
2025-12-14 18:07:08 +0300

    Create manager.js

---
+++ src/bots/whatsapp/manager.js
@@ -0,0 +1,43 @@
+/**
+ * WhatsApp Client Manager
+ * Stage 4: Linked Device
+ */
+
+const { Client, LocalAuth } = require('whatsapp-web.js');
+const path = require('path');
+
+class WhatsAppManager {
+  constructor() {
+    this.clients = new Map(); // sessionId => client
+  }
+
+  createClient(sessionId) {
+    if (this.clients.has(sessionId)) {
+      return this.clients.get(sessionId);
+    }
+
+    const client = new Client({
+      authStrategy: new LocalAuth({
+        clientId: sessionId,
+        dataPath: path.join(process.cwd(), 'sessions')
+      }),
+      puppeteer: {
+        headless: true,
+        args: [
+          '--no-sandbox',
+          '--disable-setuid-sandbox',
+          '--disable-dev-shm-usage'
+        ]
+      }
+    });
+
+    this.clients.set(sessionId, client);
+    return client;
+  }
+
+  getClient(sessionId) {
+    return this.clients.get(sessionId);
+  }
+}
+
+module.exports = new WhatsAppManager();
